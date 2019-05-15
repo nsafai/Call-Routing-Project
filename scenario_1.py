d33f3f8292a68df1c94414ad42df050183ede255
Nicolai Safai <nicolaisafai@gmail.com>
2019-05-15 12:58:54 -0700

    updating names of output files

--- scenario_1.py
+++ scenario_1.py
@@ -3,10 +3,10 @@
 # Step 0: Copy full phone number including '+' and ',', for example '+14105548067,'.
 # Step 1: Open routes file.
 # Step 2: Search for phone number using CMD+F (or CTRL+F on windows).
-#         If there are no search results, hit backspace in search bar befor the ','.
+#         If there are no search results, remove the last digit in search bar before the ','
 #         Ex: '+1410554806,' '+141055480,' '+14105548,' '+1410554,'
 # Step 3: Continue repeating Step (2) until you get a match.
 #         In the unlikely event you get several matches, choose the cheapest.
+#         If you keep deleting numbers until you only have a '+,' left, there's no match
 # Step 4: You're done!
 #         - If you found a match in Step (3), the cost is the number on the right side of the comma.
-#         - Otherwise, we don't have a matching route.
